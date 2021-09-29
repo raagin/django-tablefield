a60d9b96a4df55ee617c730c4f2de9a10693eaa9
Yury Lapshinov <y.raagin@gmail.com>
2021-09-29 15:22:00 +0300

    fix and simplify colspan/rowspan cases

--- tablefield/templatetags/tablefield_tags.py
+++ tablefield/templatetags/tablefield_tags.py
@@ -23,30 +23,24 @@ def tablefield_render(value):
     # iterate over all table [['1', None, None], ['1', '2', '3']]
     # and replace values with objects with extra data - mergeCells and cellsMeta.    
     # and drop next cells when colspan or rowspan exists.
-    colspan_count = 0
-    rowspan_count = 0
 
     for ridx, row in enumerate(data):
-        colspan_count += rowspan_count
-        rowspan_count = 0
         for cidx, col in enumerate(row):
             colspan, rowspan = 0, 0
 
             if cidx == 0:
                 data_objects.append([])
 
-            # Drop next cells when colspan or rowspan exists.
-            if colspan_count > 0 and not col:
-                colspan_count = colspan_count - 1
+            # Drop next cells when colspan or rowspan exists. 
+            # Now just check if cell equal None.
+            if col == None:
                 continue
             
             # find by cells position (row, col) in merge_cells
             merge_сell = next((x for x in merge_сells if x['row'] == ridx and x['col'] == cidx), None)
             if merge_сell:
-                colspan = merge_сell.get('colspan', 0)
-                colspan_count += colspan - 1
-                rowspan = merge_сell.get('rowspan', 0)
-                rowspan_count += rowspan - 1
+                colspan = merge_сell.get('colspan', 1)
+                rowspan = merge_сell.get('rowspan', 1)
 
             # find by cells position (row, col) in cells_meta
             cell_meta = next((x for x in cells_meta if x['row'] == ridx and x['col'] == cidx), None)
@@ -58,7 +52,6 @@ def tablefield_render(value):
                 'rowspan': rowspan,
                 'className': class_name
                 })
-
 
     table_header = data_objects.pop(0) if first_row_is_table_header else None
 
